12b53ce9def2a09f3daf7820c3331c7ec3a61ccc
Bernardo B <bernardo.acosta.barbosa@GMAIL.COM>
2025-12-02 14:00:50 -0500

    Add files via upload

---
+++ Clothing.java
@@ -0,0 +1,10 @@
+//enum size
+//colour
+public class Clothing extends Item{
+    public Clothing(String name, String desc, float price, int stock) {
+        super(name, desc, price, stock);
+    }
+
+
+}
+    
---
+++ Prepackaged.java
@@ -0,0 +1,10 @@
+//nutrition
+//
+public class Prepackaged extends Item{
+    public Prepackaged(String name, String desc, float price, int stock) {
+        super(name, desc, price, stock);
+    }
+
+
+}
+    
