600acf0e3446f8a73d83a0236910fea2406bb501
DarkSnow082 <aaronlewis083@gmail.com>
2025-12-04 13:28:07 -0500

    nothing2

--- Grocery.java
+++ Grocery.java
@@ -1,11 +1,25 @@
-//nutrition
-//kgs left / amount left
-//each item of grocery will have same weight or random?
-public class Grocery extends Item{
-    public Grocery(String name, String desc, float price, int stock) {
-        super(name, desc, price, stock);
+import java.time.LocalDate;
 
-    
+public class Grocery extends Item {
+    private float weightKg;     
+    private LocalDate expiryDate;
+
+    public Grocery(String name, String description, String brand,
+                   float pricePerKg, float weightKg, LocalDate expiryDate) {
+        super(name, description, brand, pricePerKg, 1); 
+        this.weightKg = weightKg;
+        this.expiryDate = expiryDate;
     }
-    
+
+    // Customer buys a certain weight
+    public boolean takeWeight(float amountKg) {
+        if (amountKg <= 0 || amountKg > weightKg) return false;
+        weightKg -= amountKg;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " | Weight: " + weightKg + "kg | Expires: " + expiryDate;
+    }
 }
